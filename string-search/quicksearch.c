41afacbb55ea0ac245385854b701956e8a73f9db
Tommy M. McGuire <mcguire@crsr.net>
2017-06-19 21:03:47 -0500

    Replace ASIZE with CHARS

--- string-search/quicksearch.c
+++ string-search/quicksearch.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-#define ASIZE 256
+#define CHARS 256
 
 void
-make_bad_char_shift (char *needle, int n, int bad_char_shift[])
+make_bad_char_shift (char *needle, int n, int bad_char_shift[], int chars)
 {
   int i;
 
-  for (i = 0; i < ASIZE; ++i) {
+  for (i = 0; i < chars; ++i) {
     bad_char_shift[i] = n + 1;
   }
   for (i = 0; i < n; ++i) {
@@ -18,10 +18,10 @@ make_bad_char_shift (char *needle, int n, int bad_char_shift[])
 int
 QS (char *needle, int n, char *haystack, int h)
 {
-  int i, j, bad_char_shift[ASIZE];
+  int i, j, bad_char_shift[CHARS];
 
   /* Preprocessing */
-  make_bad_char_shift (needle, n, bad_char_shift);
+  make_bad_char_shift (needle, n, bad_char_shift, CHARS);
 
   /* Searching */
   i = 0;
@@ -39,8 +39,11 @@ QS (char *needle, int n, char *haystack, int h)
 int
 main(int argc, char *argv[])
 {
-    printf("DEF in ABCDEFG: %d\n", QS("DEF", 3, "ABCDEFG", 7));
-    printf("DEL in ABCDEFG: %d\n", QS("DEL", 3, "ABCDEFG", 7));
+  printf("DEF in ABCDEFG: %d\n", QS("DEF", 3, "ABCDEFG", 7));
+  printf("DEL in ABCDEFG: %d\n", QS("DEL", 3, "ABCDEFG", 7));
+  printf("ABCB in XXXXXXXXX: %d\n", QS("ABCB", 4, "XXXXXXXXX", 9));
+  printf("ABCB in XXXXXABCB: %d\n", QS("ABCB", 4, "XXXXXABCB", 9));
+  printf("ABCB in XXXABCBXX: %d\n", QS("ABCB", 4, "XXXABCBXX", 9));
 }
 #endif
 
