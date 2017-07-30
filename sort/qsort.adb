3bea4476fe45fffed8513d754a569e1f44f214bb
Tommy M. McGuire <mcguire@crsr.net>
2017-07-30 00:29:42 -0500

    Proved partitioning preserves LEq.
    Some formatting, additional useless comments.

--- sort/qsort.adb
+++ sort/qsort.adb
@@ -4,9 +4,9 @@ package body QSort with SPARK_Mode is
 
    function Is_Partitioned
      (Ary : in Nat_Array;
-      L : Natural;
-      P : Natural;
-      U : Natural) return Boolean
+      L   : Natural;
+      P   : Natural;
+      U   : Natural) return Boolean
    is
      ((for all K in L .. P => Ary(K) <= Ary(P))
       and then (for all K in P + 1 .. U => Ary(P) < Ary(K)))
@@ -15,15 +15,47 @@ package body QSort with SPARK_Mode is
            and then L <= P and then P <= U and then U < Natural'Last),
    Ghost;
 
+--     procedure Lemma_LEq_Transitive
+--       (A, B, C : in Nat_Array;
+--        L, U : in Natural)
+--       with
+--         Ghost,
+--         Pre => (A'First = B'First and then A'Last = B'Last
+--                 and then A'First = C'First and then A'Last = C'Last
+--                 and then L in A'Range and then U in A'Range
+--                 and then L <= U and then U < Natural'Last
+--                 and then (for all V in Natural'Range =>
+--                             ((if LEq(A,L,U,V) then LEq(B,L,U,V))
+--                              and then (if LEq(B,L,U,V) then LEq(C,L,U,V))))),
+--       Post => (for all V in Natural'Range =>
+--                  (if LEq(A,L,U,V) then LEq(C,L,U,V)))
+--     is
+--        MaxA : Natural;
+--        I : Natural := L;
+--     begin
+--        MaxA := A(I);
+--        while I <= U loop
+--           pragma Loop_Invariant (L <= I and then I <= U);
+--           pragma Loop_Invariant (for all K in L .. I - 1 => A(K) <= MaxA);
+--           if MaxA < A(I) then
+--              MaxA := A(I);
+--           end if;
+--           I := I + 1;
+--        end loop;
+--        pragma assert (LEq(A, L, U, MaxA));
+--        pragma assert (LEq(B, L, U, MaxA));
+--        pragma assert (LEq(C, L, U, MaxA));
+--     end Lemma_LEq_Transitive;
+
    procedure Swap
-     (Values : in out Nat_Array;
-      I : in Natural;
-      J : in Natural;
+     (Values   : in out Nat_Array;
+      I        : in Natural;
+      J        : in Natural;
       Original : in Nat_Array;
-      Witness : in out Nat_Array)
+      Witness  : in out Nat_Array)
      with
-       Pre => (I in Values'Range and then J in Values'Range
-               and then Is_Permutation(Values, Original, Witness)),
+       Pre  => (I in Values'Range and then J in Values'Range
+                and then Is_Permutation(Values, Original, Witness)),
        Post => (Values(I) = Values'Old(J)
                 and then Values(J) = Values'Old(I)
                 and then (for all K in Values'Range =>
@@ -33,13 +65,34 @@ package body QSort with SPARK_Mode is
    is
       Temp : Natural;
    begin
-      Temp := Values(I);
-      Values(I) := Values(J);
-      Values(J) := Temp;
-      Temp := Witness(I);
+      Temp       := Values(I);
+      Values(I)  := Values(J);
+      Values(J)  := Temp;
+      Temp       := Witness(I);
       Witness(I) := Witness(J);
       Witness(J) := Temp;
    end Swap;
+
+--     procedure Lemma_Swap_Preserve_LEq
+--       (Pre, Post : in Nat_Array;
+--        L, U : in Natural;
+--        I, J : in Natural)
+--       with
+--         Ghost,
+--         Pre => (Pre'First = Post'First and then Pre'Last = Post'Last
+--                 and then L in Pre'Range and then U in Pre'Range
+--                 and then L <= I and then I <= J and then J <= U
+--                 and then Post(I) = Pre(J)
+--                 and then Post(J) = Pre(I)
+--                 and then (for all K in L .. U =>
+--                             (K = I or else K = J
+--                              or else Post(K) = Pre(K)))),
+--       Post => (for all V in Natural'Range =>
+--                  (if LEq(Pre,L,U,V) then LEq(Post,L,U,V)))
+--     is
+--     begin
+--        null;
+--     end Lemma_Swap_Preserve_LEq;
 
    procedure Midpoint
      (Values : in Nat_Array;
@@ -55,42 +108,54 @@ package body QSort with SPARK_Mode is
    end Midpoint;
 
    procedure Partition
-     (Values : in out Nat_Array;
-      L : in Natural;
-      U : in Natural;
-      P : out Natural;
+     (Values   : in out Nat_Array;
+      L        : in Natural;
+      U        : in Natural;
+      P        : out Natural;
       Original : in Nat_Array;
-      Witness : in out Nat_Array)
+      Witness  : in out Nat_Array)
      with
-       Pre => (L in Values'Range and then U in Values'Range
-               and then L < U and then U < Natural'Last
-               and then Is_Permutation(Values, Original, Witness)),
+       Pre  => (L in Values'Range and then U in Values'Range
+                and then L < U and then U < Natural'Last
+                and then Is_Permutation(Values, Original, Witness)),
        Post => (L <= P and then P <= U
                 and then Is_Permutation(Values, Original, Witness)
-                and then Is_Partitioned(Values, L, P, U))
+                and then Is_Partitioned(Values, L, P, U)
+                and then (for all V in Natural'Range =>
+                            (if LEq(Values'Old, L, U, V)
+                                 then LEq(Values, L, U, V))))
    is
-      I : Natural := L;
-      J : Natural := U;
-      M : Natural;
+      I       : Natural   := L;
+      J       : Natural   := U;
+      M       : Natural;
+      Initial : Nat_Array := Values with Ghost;
    begin
       Midpoint(Values, L, U, M);
       Swap(Values, L, M, Original, Witness);
+
       while I < J loop
          pragma Loop_Invariant (L <= I and I <= J and J <= U);
          pragma Loop_Invariant (Is_Permutation(Values, Original, Witness));
          pragma Loop_Invariant (for all K in L .. I - 1 => Values(K) <= Values(L));
          pragma Loop_Invariant (for all K in J + 1 .. U => Values(L) < Values(K));
+         pragma Loop_Invariant (for all V in Natural'Range =>
+                                  (if LEq(Initial, L, U, V)
+                                   then LEq(Values, L, U, V)));
+
          while I < J and then Values(I) <= Values(L) loop
             pragma Loop_Invariant (L <= I and I <= J and J <= U);
             pragma Loop_Invariant (for all K in L .. I => Values(K) <= Values(L));
             I := I + 1;
          end loop;
          pragma assert (for all K in L .. I - 1 => Values(K) <= Values(L));
+
          while I < J and then Values(L) < Values(J) loop
             pragma Loop_Invariant (L <= I and I <= J and J <= U);
             pragma Loop_Invariant (for all K in J .. U => Values(L) < Values(K));
             J := J - 1;
          end loop;
+         pragma assert (for all K in J + 1 .. U => Values(L) < Values(K));
+
          if I < J then
             pragma assert (Values(I) > Values(L) and Values(L) >= Values(J));
             Swap(Values, I, J, Original, Witness);
@@ -104,18 +169,13 @@ package body QSort with SPARK_Mode is
       end if;
       pragma assert (L <= I);
 
-      pragma assert (for all K in L .. I - 1 => Values(K) <= Values(L));
-      pragma assert (Values(I) <= Values(L));
-      pragma assert (for all K in L .. I => Values(K) <= Values(L));
-
-      pragma assert (for all K in J + 1.. U => Values(L) < Values(K));
-      pragma assert (I = U or else Values(L) < Values(I + 1));
-      pragma assert (for all K in I + 1 .. U => Values(L) < Values(K));
-
       Swap(Values, L, I, Original, Witness);
 
       pragma assert (for all K in L .. I => Values(K) <= Values(I));
       pragma assert (for all K in I + 1 .. U => Values(I) < Values(K));
+
+      pragma assert (for all V in Natural'Range => (if LEq(Initial, L, U, V)
+                                                    then LEq(Values, L, U, V)));
 
       P := I;
    end Partition;
@@ -123,11 +183,11 @@ package body QSort with SPARK_Mode is
    ------------------------------------
 
    procedure Sort
-     (Values : in out Nat_Array;
-      L : in Natural;
-      U : in Natural;
+     (Values   : in out Nat_Array;
+      L        : in Natural;
+      U        : in Natural;
       Original : in Nat_Array;
-      Witness : in out Nat_Array)
+      Witness  : in out Nat_Array)
    is
       P : Natural;
    begin
--- sort/qsort.ads
+++ sort/qsort.ads
@@ -3,9 +3,9 @@ package QSort with SPARK_Mode is
    type Nat_Array is array (Natural range <>) of Natural;
 
    function Is_Permutation
-     (Ary : in Nat_Array;
+     (Ary      : in Nat_Array;
       Original : in Nat_Array;
-      Witness : in Nat_Array) return Boolean
+      Witness  : in Nat_Array) return Boolean
    is
       (Ary'First = Original'First and then Ary'Last = Original'Last
        and then Ary'First = Witness'First and then Ary'Last = Witness'Last
@@ -13,23 +13,33 @@ package QSort with SPARK_Mode is
        and then (for all K in Ary'Range => Ary(K) = Original( Witness(K) )))
    with Ghost;
 
-   function Is_Sorted(Ary : in Nat_Array) return Boolean
+   function Is_Sorted(A : in Nat_Array) return Boolean
    is
-     (for all P in Ary'Range =>
-        (for all Q in P .. Ary'Last =>
-              Ary(P) <= Ary(Q)))
+     (for all P in A'Range => (for all Q in P .. A'Last => A(P) <= A(Q)))
    with Ghost;
 
+   function LEq
+     (Values : in Nat_Array;
+      L      : in Natural;
+      U      : in Natural;
+      Value  : in Natural) return Boolean
+   is
+     (for all K in L .. U => Values(K) <= Value)
+   with
+   Pre => (L in Values'Range and then U in Values'Range),
+   Ghost;
+
    procedure Sort
-     (Values : in out Nat_Array;
-      L : in Natural;
-      U : in Natural;
+     (Values   : in out Nat_Array;
+      L        : in Natural;
+      U        : in Natural;
       Original : in Nat_Array;
-      Witness : in out Nat_Array)
+      Witness  : in out Nat_Array)
      with
-       Pre => (L in Values'Range and then U in Values'Range
-               and then U < Natural'Last
-               and then Is_Permutation(Values, Original, Witness)),
+       Pre  => (L in Values'Range and then U in Values'Range
+                and then U < Natural'Last
+                and then Is_Permutation(Values, Original, Witness)),
        Post => (Is_Permutation(Values, Original, Witness));
+
 
 end QSort;
